c13d6a08ea445ac1534ac8fea8aaef8487699e38
ntpadovani <normant_padovani@hotmail.com>
2023-01-30 18:40:55 -0400

    Task 1 for the python_classes project

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+" Write a class Square that defines a square by: (based on 0-square.py) "
+
+
+class Square:
+    """
+    Square class
+
+    Attributes:
+        size (no type/value verification) : Private instance attribute
+    """
+    def __init__(self, size):
+        self.__size = size
