12f6ae20851638f5795da5ebc6554f5b0cc55ea3
ntpadovani <normant_padovani@hotmail.com>
2023-01-30 18:42:08 -0400

    Task 2 for the python_classes project

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+" Write a class Square that defines a square by: (based on 0-square.py) "
+
+
+class Square:
+    """
+    Square class
+
+    Attributes:
+        size (int) : Private instance attribute, size of the square
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
