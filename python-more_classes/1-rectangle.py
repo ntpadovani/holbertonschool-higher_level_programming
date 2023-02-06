df76f20c44e7aa8cdcdf1cb8286618775d293ef4
ntpadovani <normant_padovani@hotmail.com>
2023-02-06 14:11:29 -0400

    wrote Type instead of type

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -12,13 +12,13 @@ class Rectangle:
     """
 
     def __init__(self,width=0,height=0):
-        if Type(width) is not int:
+        if type(width) is not int:
             raise TypeError('width must be an integer')
         elif width < 0:
             raise ValueError('width must be >= 0')
         else:
             self.__width = width
-        if Type(height) is not int:
+        if type(height) is not int:
             raise TypeError('height must be an integer')
         elif height < 0:
             raise ValueError('height must be >= 0')
@@ -31,7 +31,7 @@ def width(self):
 
 @width.setter
 def width(self, value):
-    if Type(value) is not int:
+    if type(value) is not int:
         raise TypeError('width must be an integer')
     elif value < 0:
         raise ValueError('width must be >= 0')
@@ -44,7 +44,7 @@ def height(self):
 
 @height.setter
 def height(self, value):
-        if Type(height) is not int:
+        if type(height) is not int:
             raise TypeError('height must be an integer')
         elif height < 0:
             raise ValueError('height must be >= 0')
