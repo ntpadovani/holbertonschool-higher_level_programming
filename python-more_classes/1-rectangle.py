45b92f3df59597c3e5213d043c2630499e48cd34
ntpadovani <normant_padovani@hotmail.com>
2023-02-06 14:09:35 -0400

    coding error

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -30,19 +30,20 @@ def width(self):
     return self.__width
 
 @width.setter
-def width(self,value):
+def width(self, value):
     if Type(value) is not int:
         raise TypeError('width must be an integer')
     elif value < 0:
-        raise ValueError('width mus be >= 0')
+        raise ValueError('width must be >= 0')
     else:
         self.__width = value
+
 @property
 def height(self):
     return self.__height
 
 @height.setter
-def height(self,value):
+def height(self, value):
         if Type(height) is not int:
             raise TypeError('height must be an integer')
         elif height < 0:
