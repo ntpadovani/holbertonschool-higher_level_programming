7143ffece1d7eb3e63827a4e8a102e87c52a7fab
ntpadovani <normant_padovani@hotmail.com>
2023-02-06 14:04:37 -0400

    coding error

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -28,23 +28,25 @@ class Rectangle:
 @property
 def width(self):
     return self.__width
+
 @width.setter
-    def width(self,value):
+def width(self,value):
     if Type(value) is not int:
         raise TypeError('width must be an integer')
     elif value < 0:
         raise ValueError('width mus be >= 0')
     else:
-        return self.width = value
+        self.__width = value
 @property
 def height(self):
     return self.__height
+
 @height.setter
-    def height(self,value):
+def height(self,value):
         if Type(height) is not int:
             raise TypeError('height must be an integer')
         elif height < 0:
             raise ValueError('height must be >= 0')
         else:
-            return self.height = value
+            self.__height = value
 
