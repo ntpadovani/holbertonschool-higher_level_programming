12cc8772eab4e2886eab0c3376d3ebba4a3ec3da
ntpadovani <normant_padovani@hotmail.com>
2023-04-18 15:54:47 -0400

    Task 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -5,17 +5,14 @@ const argu = process.argv;
 let count = 0;
 
 if (argu !== null) {
-	//for (let idx = 0; argu[idx] !== '/0'; idx++){
-	//count++;
-	for (idx in argu)
-		count++;
-  //}
+  // for (let idx = 0; argu[idx] !== '/0'; idx++){
+  // count++;
+  for (count in argu) { count++; }
+  // }
 }
 
 if (count === 3 || count > 2) {
   console.log(argu[2]);
-}
-
-else if (count <= 2) {
-  console.log('No Argument');
+} else if (count <= 2) {
+  console.log('No argument');
 }
