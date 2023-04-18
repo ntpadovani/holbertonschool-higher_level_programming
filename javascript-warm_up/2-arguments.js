e52f7c656810a1e9058386e27d3eeaa027766f3d
ntpadovani <normant_padovani@hotmail.com>
2023-04-18 11:40:53 -0400

    Task2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const process = require('process');
+const argu = process.argv;
+
+if (argu.length == 2)
+{
+	console.log('No argument');
+}
+if (argu.length == 3)
+{
+	console.log('Argument found');
+}
+if (argu.length > 3)
+{
+	console.log('Arguments found');
+}
